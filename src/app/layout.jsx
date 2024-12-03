ee4061f24be64b47f356d8be857d5c91536813de
feyzsharaf <feyzullayevsharafx1@protonmail.com>
2024-12-03 20:16:54 +0400

    added home page ui

---
+++ src/app/layout.jsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Tajeer",
+  description: "an e-commerce website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="m-0 p-0 bg-black text-white">
+        {children}
+      </body>
+    </html>
+  );
+}
